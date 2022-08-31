5f1d66587eb177a339ff2d5a23a1be201156db87
victor daniel quino jimenez <daniel@victors-MacBook-Pro.local>
2022-08-30 23:40:00 -0400

    update: config base

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
+import { ClassSerializerInterceptor } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // FILTRO PARA CAPTURAR ERRORES
+  app.useGlobalFilters(new AllExceptionFilter());
+
+  // INTERCEPTOR: PARA QUE LA CONSULTA A UNA API NO DURE MAS DE 2 MIN
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
